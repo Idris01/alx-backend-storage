5f52c390aa0af821017807271430c88de90473b6
Idris Adebowale <idrys01@gmail.com>
2023-12-15 04:08:44 +0000

    create table safely

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Creates table named user with attributes id, email
 -- and name
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) UNIQUE NOT NULL,
 	name VARCHAR(255)
