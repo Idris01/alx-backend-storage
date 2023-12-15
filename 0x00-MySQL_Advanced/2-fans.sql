4caed9daa77eb2c708b8dde51959c893c11f8151
Idris Adebowale <idrys01@gmail.com>
2023-12-15 09:38:43 +0000

    add explicit grouping and ordering

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- Ranks country origin of bands, ordered by the number of
 -- non-unique fans
 SELECT origin, COUNT(*) nb_fans FROM metal_bands
-GROUP BY 1 ORDER BY 2 DESC;
+GROUP BY origin ORDER BY nb_fans DESC;
