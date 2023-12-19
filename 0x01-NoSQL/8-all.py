31124bcb885b442ae4fda130fc4eca56ef982706
Idris Adebowale <idrys01@gmail.com>
2023-12-19 09:05:31 -0800

    add python function that find list of all documents in a mongodb collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Define a function that list documents in a mongodb collection
+"""
+
+
+def list_all(mongo_collection):
+    """List documents in a given collection
+
+    Arguments:
+    =========
+    mongo_collection - a mongodc collection
+
+    Returns - List of documents if any or empty list
+    """
+    documents = mongo_collection.find()
+    return list(documents)
